0de1851a820cdb035f93493a1c9da8820a3756eb
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2024-02-06 19:04:05 +0600

    Added Header, footer and hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Footer />
+    </>
+  )
 }
 
 export default App;
