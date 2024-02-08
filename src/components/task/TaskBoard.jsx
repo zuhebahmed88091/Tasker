8edd9b269d67dd0fe98a5b929a1c282557e1396c
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2024-02-08 14:26:06 +0600

    Rendering the primary task list

--- src/components/task/TaskBoard.jsx
+++ src/components/task/TaskBoard.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskLists from "./TaskLists";
 
 export default function TaskBoard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React",
+    description: "I want to learn React to build a web application.",
+    tags: ["react", "webdev", "javascript"],
+    priority: "High",
+    isFavorite: true,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
@@ -12,7 +22,7 @@ export default function TaskBoard() {
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskActions />
-          <TaskLists />
+          <TaskLists tasks={tasks} />
         </div>
       </div>
     </section>
