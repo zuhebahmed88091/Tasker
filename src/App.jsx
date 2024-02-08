f7b82b856425f0d98655f8e6c95d48db54886095
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2024-02-08 10:45:37 +0600

    Initial task list completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import TaskBoard from "./components/task/TaskBoard";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <TaskBoard />
       <Footer />
     </>
   )
